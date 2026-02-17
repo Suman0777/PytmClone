f44e307832f7f7eb5dabc00ae2c5eccbe2e23c9e
suman basak <gorangobasakpkr@gmail.com>
2026-02-17 23:09:25 +0530

    Add the Login code for backend

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -12,7 +12,6 @@ router.get("/", (req, res)=>{
 // fro user sign in
 
 router.post("/signup", async (req, res)=>{
-    
     try {
         const {username, password, firstName, lastName} = req.body;
         console.log(username);
@@ -49,5 +48,35 @@ router.post("/signup", async (req, res)=>{
     }
 })
 
+// for Login 
+
+router.post("/login", async(req, res)=>{
+    const {username, password} = req.body;
+
+    const isThere = await User.findOne({username, password});
+
+    try {
+        if(!isThere){
+            return res.status(400).json({
+                msg: "userName Not Found"
+            })
+        }
+    
+        else{
+            const token = JWt.sign({userid: User._id}, JWT_SECRET_key)
+            res.status(200).json({
+                token,
+                success: true,
+                msg: "User Has login"
+            })
+        }
+    } catch (error) {
+        console.log(error.message);
+        res.status(404).json({
+            msg: error.message,
+            success: false
+        })
+    }
+})
 
 export default router
