c6effd60ed6d80b9b9ab1885a8c4f1d5f517bc7c
suman basak <gorangobasakpkr@gmail.com>
2026-02-18 02:48:16 +0530

    auth add .

---
+++ Backend/auth.js
@@ -0,0 +1,33 @@
+import { JWT_SECRET_key } from "./config.js";
+import JWT from "jsonwebtoken"
+
+const authMIddleware= (req, res, next)=>{
+    const authHeader = req.headers.authorization
+    if(!authHeader || !authHeader.statswith('barer')){
+        return res.status(400).json({
+            msg: "Token Invalid"
+        })
+    }
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const decode = JWT.verify(token, JWT_SECRET_key);
+
+        if(decode.userId){
+            req.userid = decode.userId;
+            next();
+        }
+        else{
+            res.status(403).json({
+            msg: "userId not found"
+        })
+        }
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(403).json({
+            msg: error.message
+        })
+    }
+}
+export default authMIddleware
